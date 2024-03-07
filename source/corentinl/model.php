7659cba56c6a69cd568f0c6fa80f59e966aac44c
CorentinLabelle <corentinlabelle2@gmail.com>
2024-03-07 10:25:48 -0500

    SQL Injection

--- source/corentinl/model.php
+++ source/corentinl/model.php
@@ -7,8 +7,17 @@ require("../includes/bootstrap.php");
 /*****************************************************
  *                     FUNCTIONS                     *
  *****************************************************/
-function query($query) {
+function vulnerable_query($value) {
+    global $db;
+    $sql_request = "SELECT user_firstname, user_lastname FROM users WHERE user_firstname='$value';";
+    return mysqli_query($db, $sql_request);
+}
+
+function safe_query($value) {
     global $db;
 
-    return mysqli_query($db, $query);
+    $sql_statement = $db->prepare("SELECT user_firstname, user_lastname FROM users WHERE user_firstname=?");
+    $sql_statement->bind_param("s", $value);
+    $sql_statement->execute();
+    return $sql_statement->get_result();
 }
--- source/corentinl/sql.php
+++ source/corentinl/sql.php
@@ -11,12 +11,53 @@ require("model.php");
 ?>
 
 <!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8" />
-        <title>Injection SQL</title>
-    </head>
-    <body>
-        <h1>Injection SQL</h1>
-    </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SQL Injection</title>
+</head>
+<body>
+    Enter your first name
+
+    <form method="get">
+        <input type="text" name="search_input" 
+            value="<?php echo isset($_GET['search_input']) ? $_GET['search_input'] : '' ?>">
+        <input type="submit" name="safe_search_button" value="Safe Search!" class="Button">
+        <input type="submit" name="vulnerable_search_button" value="Vulnerable Search..." class="Button">
+    </form> 
+  
+    <?php
+        if(array_key_exists('safe_search_button', $_GET)) { 
+            safe_search_button_callback();
+        }
+        elseif(array_key_exists('vulnerable_search_button', $_GET)) { 
+            vulnerable_search_button_callback();
+        }
+        
+        function safe_search_button_callback() {           
+            $sql_result = safe_query($_GET['search_input']);
+            display_sql_results($sql_result);
+        }
+
+        function vulnerable_search_button_callback() {
+            $sql_result = vulnerable_query($_GET['search_input']);
+            display_sql_results($sql_result);
+        }
+
+        function display_sql_results($sql_result) {
+            if (mysqli_num_rows($sql_result) == 0) {
+                echo "Nothing found...";
+            } 
+            else { 
+                while ($row = $sql_result->fetch_assoc()) {
+                    echo $row['user_firstname'] ." " , $row['user_lastname'] ."<br>";
+                }           
+            }
+        }
+
+    ?>
+
+</body>
 </html>
+
